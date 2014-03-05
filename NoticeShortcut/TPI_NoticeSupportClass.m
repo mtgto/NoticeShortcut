b51e2a875f431670184d296483d9dd031fb49eb3
mtgto <hogerappa@gmail.com>
2014-03-05 23:49:35 +0900

    Add the support for multi lines.

--- NoticeShortcut/TPI_NoticeSupportClass.m
+++ NoticeShortcut/TPI_NoticeSupportClass.m
@@ -13,12 +13,24 @@
 - (id)interceptUserInput:(id)input command:(NSString *)command
 {
     if ([NSEvent modifierFlags] & NSControlKeyMask && [command isEqualToString:@"PRIVMSG"]) {
+        NSString *prefix = [NSString stringWithFormat:@"/notice %@ ", self.worldController.selectedChannel.name];
         if ([input isKindOfClass:[NSAttributedString class]]) {
-            NSMutableAttributedString *newInput = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"/notice %@ ", self.worldController.selectedChannel.name]];
-            [newInput appendAttributedString:input];
+            NSString *string = [input string];
+            NSMutableAttributedString *newInput = [[NSMutableAttributedString alloc] initWithAttributedString:input];
+            for (NSInteger i=[string length]; i>=0; i--) {
+                if (i == 0 || [string characterAtIndex:i-1] == 0x2028) {
+                    [newInput insertAttributedString:[[NSAttributedString alloc] initWithString:prefix] atIndex:i];
+                }
+            }
             return newInput;
         } else {
-            return [NSString stringWithFormat:@"/notice %@ %@", self.worldController.selectedChannel.name, input];
+            NSMutableString *newInput = [[NSMutableString alloc] initWithCapacity:[input length]];
+            [(NSString *)input enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+                [newInput appendString:prefix];
+                [newInput appendString:line];
+                [newInput appendString:@"\n"];
+            }];
+            return newInput;
         }
     }
     return input;
